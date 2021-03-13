122cccb16b9496ed93d698074994231413d306f7
jakobkubicki <jakobkub@gmail.com>
2021-03-13 12:34:49 -0800

    Seventh Commit

--- app.js
+++ app.js
@@ -14,16 +14,17 @@ mongoose.connection.on('connected', () => {
     console.log("Mongo is connected");
 });
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'musify/public', 'index.html'));
-});
-
 const userRouter = require('./routes/User');
+const { get } = require('./routes/User');
 app.use('/user',userRouter);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('musify/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(_dirname, 'musify', 'build', 'index.html'));
+    });
+}
 
 app.listen(process.env.PORT || 5000,()=>{
     console.log('express server started');
